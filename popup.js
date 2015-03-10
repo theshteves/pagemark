e44e6a51b646263711d7470b8aa5cf5dce31690e
theshteves <kneisers@msu.edu>
2015-03-10 12:54:51 -0400

    Merged scripts

--- popup.js
+++ popup.js
@@ -118,3 +118,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+
+/*
+$(document).ready(function() {
+
+    // Tab switching
+    $("#create-tab").click(function() {
+	$(".list-group").hide();
+	$(".form-group").show();
+	$("#save-button").show();
+    });
+    $("#load-tab").click(function() {
+	$(".list-group").show();
+	$(".form-group").hide();
+	$("#save-button").hide();
+    });
+    $("#lt").click(function() {
+	$(".list-group").show();
+	$(".form-group").hide();
+	$("#save-button").hide();
+    });
+    //$('#count').value($('.list-group-item').length);
+
+});
+*/
